7a2e2ad755776c7fb625a1e200fef18260828a4a
dgmid <duncan@midwinter-dg.com>
2020-09-26 16:09:29 +0200

    text formatting touchbar

--- app-source/js/touchbar.js
+++ app-source/js/touchbar.js
@@ -1,13 +1,13 @@
 'use strict'
 
 
-const { TouchBar }	= require('electron')
+const { TouchBar, app }	= require('electron')
 const path			= require( 'path' )
 const nativeImage	= require('electron').nativeImage
 const log			= require( 'electron-log' )
 
-const { TouchBarButton, TouchBarSpacer } = TouchBar
-let previewTouchbar, editTouchbar
+const { TouchBarButton, TouchBarSpacer, TouchBarPopover } = TouchBar
+let previewBar, editBar, formatBar
 
 
 module.exports = {
@@ -16,7 +16,14 @@ module.exports = {
 		
 		const i18n = require( './i18n.min' )
 		
-		previewTouchbar = new TouchBar({
+		previewBar = new TouchBar({
+			
+			escapeItem: new TouchBarButton({
+				
+				accessibilityLabel: i18n.t('touchbar:preview.edit', 'Edit'),
+				icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/edit_2x.png') ),
+				click: () => { window.webContents.send( 'note', 'edit' ) }
+			}),
 			
 			items: [
 				
@@ -64,11 +71,96 @@ module.exports = {
 					accessibilityLabel: i18n.t('touchbar:preview.categories', 'Categories'),
 					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/categories_2x.png') ),
 					click: () => { window.webContents.send( 'toggle-categories', '' ) }
+				}),
+				
+				new TouchBarSpacer({ size: 'small' }),
+				
+				new TouchBarButton({
+					
+					accessibilityLabel: i18n.t('touchbar:preview.prefercences', 'Preferences'),
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/preferences_2x.png') ),
+					click: () => { app.emit('open-prefs') }
 				})
 			]
 		})
 		
-		editTouchbar = new TouchBar({
+		formatBar = new TouchBar({
+			
+			items: [
+				
+				new TouchBarButton({
+					
+					accessibilityLabel: i18n.t('touchbar:format.heading', 'Heading'),
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/heading_2x.png') ),
+					click: () => {  }
+				}),
+				
+				new TouchBarButton({
+					
+					accessibilityLabel: i18n.t('touchbar:format.heading', 'Bold'),
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/bold_2x.png') ),
+					click: () => { window.webContents.send('markdown', 'b') }
+				}),
+				
+				new TouchBarButton({
+					
+					accessibilityLabel: i18n.t('touchbar:format.heading', 'Italic'),
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/italic_2x.png') ),
+					click: () => { window.webContents.send('markdown', 'i') }
+				}),
+				
+				new TouchBarButton({
+					
+					accessibilityLabel: i18n.t('touchbar:format.ul', 'List'),
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/ul_2x.png') ),
+					click: () => { window.webContents.send('markdown', 'ul') }
+				}),
+				
+				new TouchBarButton({
+					
+					accessibilityLabel: i18n.t('touchbar:format.check', 'Checkbox List'),
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/checkbox_2x.png') ),
+					click: () => { window.webContents.send('markdown', 'cl') }
+				}),
+				
+				new TouchBarButton({
+					
+					accessibilityLabel: i18n.t('touchbar:format.link', 'Link'),
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/link_2x.png') ),
+					click: () => { window.webContents.send('markdown', 'a') }
+				}),
+				
+				new TouchBarButton({
+					
+					accessibilityLabel: i18n.t('touchbar:format.image', 'Image'),
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/image_2x.png') ),
+					click: () => { window.webContents.send('markdown', 'img') }
+				}),
+				
+				new TouchBarButton({
+					
+					accessibilityLabel: i18n.t('touchbar:format.code', 'Code'),
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/code_2x.png') ),
+					click: () => { window.webContents.send('markdown', 'code') }
+				}),
+				
+				new TouchBarButton({
+					
+					accessibilityLabel: i18n.t('touchbar:format.table', 'Table'),
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/table_2x.png') ),
+					click: () => { window.webContents.send('markdown', 'table') }
+				})
+			]
+		})
+		
+		editBar = new TouchBar({
+			
+			escapeItem: new TouchBarButton({
+				
+				accessibilityLabel: i18n.t('touchbar:edit.preview', 'Preview'),
+				icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/preview_2x.png') ),
+				click: () => { window.webContents.send( 'note', 'edit' ) }
+			}),
 			
 			items: [
 			
@@ -77,22 +169,15 @@ module.exports = {
 					accessibilityLabel: i18n.t('touchbar:edit.save', 'Save'),
 					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/save_2x.png') ),
 					click: () => { window.webContents.send( 'note', 'save' ) }
+				}),
+				
+				new TouchBarPopover({
+					
+					icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/format_2x.png') ),
+					showCloseButton: true,
+					items: formatBar
 				})
 			]
-		})
-		
-		previewTouchbar.escapeItem = new TouchBarButton({
-			
-			accessibilityLabel: i18n.t('touchbar:preview.edit', 'Edit'),
-			icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/edit_2x.png') ),
-			click: () => { window.webContents.send( 'note', 'edit' ) }
-		}),
-		
-		editTouchbar.escapeItem = new TouchBarButton({
-			
-			accessibilityLabel: i18n.t('touchbar:edit.preview', 'Preview'),
-			icon: nativeImage.createFromPath( path.join(__dirname, '../assets/png/preview_2x.png') ),
-			click: () => { window.webContents.send( 'note', 'edit' ) }
 		})
 		
 		module.exports.switchTouchbar( window, state )
@@ -102,11 +187,11 @@ module.exports = {
 		
 		if( state ) {
 			
-			window.setTouchBar( editTouchbar )
+			window.setTouchBar( editBar )
 		
 		} else {
 			
-			window.setTouchBar( previewTouchbar )
+			window.setTouchBar( previewBar )
 		}
 	}
 }
